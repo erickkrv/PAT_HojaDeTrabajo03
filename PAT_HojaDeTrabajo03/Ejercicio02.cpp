0db78674a397d9331722e28572d7f2c0c36c8106
erickkrv <rivaal04@gmail.com>
2023-11-20 15:06:10 -0600

    ej2

--- PAT_HojaDeTrabajo03/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio02.cpp
@@ -1,6 +1,23 @@
 #include "Ejercicio02.h"
-
+#include <map>
+#include <algorithm>
+using namespace std;
 vector<vector<string>>* Ejercicio02::groupAnagrams(vector<string>& strings)
 {
-    return nullptr;
+vector<vector<string>>* result = new vector<vector<string>>();
+	map<string, vector<string>> res;
+
+	for (string s : strings)
+	{
+		string sorted = s;
+		sort(sorted.begin(), sorted.end());
+		res[sorted].push_back(s);
+	}
+
+	for (auto it = res.begin(); it != res.end(); ++it)
+	{
+		result->push_back(it->second);
+	}
+
+	return result;
 }
