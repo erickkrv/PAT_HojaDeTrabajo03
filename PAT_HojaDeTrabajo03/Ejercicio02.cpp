a42186dbc0db04bd039f817ab503225518fb7995
erickkrv <rivaal04@gmail.com>
2023-11-24 18:58:08 -0600

    correcciones

--- PAT_HojaDeTrabajo03/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio02.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 vector<vector<string>>* Ejercicio02::groupAnagrams(vector<string>& strings)
 {
-vector<vector<string>>* result = new vector<vector<string>>();
+vector<vector<string>>* final = new vector<vector<string>>();
 	map<string, vector<string>> res;
 
 	for (string s : strings)
 	{
-		string sorted = s;
-		sort(sorted.begin(), sorted.end());
-		res[sorted].push_back(s);
+		string aOrdenar = s;
+		sort(aOrdenar.begin(), aOrdenar.end());
+		res[aOrdenar].push_back(s);
 	}
 
 	for (auto it = res.begin(); it != res.end(); ++it)
 	{
-		result->push_back(it->second);
+		final->push_back(it->second);
 	}
 
-	return result;
+	return final;
 }
--- PAT_HojaDeTrabajo03/Ejercicio03.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio03.cpp
@@ -2,5 +2,24 @@
 
 Node<int>* Ejercicio03::copyList(Node<int>* head)
 {
-    return nullptr;
+	Node<int>* head2 = nullptr;
+	Node<int>* tail2 = nullptr;
+	Node<int>* aux = head;
+
+	while(aux != nullptr)
+	{
+		Node<int>* nodo2 = new Node<int>{aux->value};
+		if(head2 == nullptr)
+		{
+			head2 = nodo2;
+			tail2 = nodo2;
+		}
+		else
+		{
+			tail2->next = nodo2;
+			tail2 = nodo2;
+		}
+		aux = aux->next;
+	}
+	return head2;
 }
