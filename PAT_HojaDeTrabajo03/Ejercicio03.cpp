eadfe895ec0739f75fa870570c35aff4bd4809a4
erickkrv <rivaal04@gmail.com>
2023-11-24 19:02:04 -0600

    ej3

--- PAT_HojaDeTrabajo03/Ejercicio03.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio03.cpp
@@ -1,25 +1,24 @@
 #include "Ejercicio03.h"
-
+#include <unordered_map>
+using namespace std;
 Node<int>* Ejercicio03::copyList(Node<int>* head)
 {
-	Node<int>* head2 = nullptr;
-	Node<int>* tail2 = nullptr;
-	Node<int>* aux = head;
+    if (!head) return nullptr;
 
-	while(aux != nullptr)
-	{
-		Node<int>* nodo2 = new Node<int>{aux->value};
-		if(head2 == nullptr)
-		{
-			head2 = nodo2;
-			tail2 = nodo2;
-		}
-		else
-		{
-			tail2->next = nodo2;
-			tail2 = nodo2;
-		}
-		aux = aux->next;
-	}
-	return head2;
+    std::unordered_map<Node<int>*, Node<int>*> nodeMap;
+
+    Node<int>* current = head;
+    while (current) {
+        nodeMap[current] = new Node<int>{current->value};
+        current = current->next;
+    }
+
+    current = head;
+    while (current) {
+        nodeMap[current]->next = nodeMap[current->next];
+        nodeMap[current]->random = nodeMap[current->random];
+        current = current->next;
+    }
+
+    return nodeMap[head];
 }
